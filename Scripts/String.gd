520e409dbb0b686713cde86bcede22c32334b2ba
turtlewit <turtlewit@gmail.com>
2019-01-26 12:39:16 -0800

    player has a string ayyyyy

--- Scripts/String.gd
+++ Scripts/String.gd
@@ -6,12 +6,34 @@ export(PackedScene) var root_node
 export(PackedScene) var section_node
 
 var sections_list = []
-
+var positions = PoolVector2Array()
 func _ready():
-	spawn_nodes()
+	#spawn_nodes()
+	pass
+	sections_list = [
+		$String_Root,
+		$String_Section,
+		$String_Section2,
+		$String_Section3,
+		$String_Section4,
+		$String_Section5
+	]
 
 func _process(delta):
-	pass
+	#translate(Vector2(100 * delta, 0))
+	update_positions()
+	update()
+
+func _draw():
+	draw_multiline(positions, Color(0, 0, 0))
+
+func update_positions():
+	positions = PoolVector2Array()
+	positions.append(sections_list[0].position)
+	for i in range(1, len(sections_list)):
+		positions.append(sections_list[i].position)
+		positions.append(sections_list[i].position)
+	positions.remove(len(positions) - 1)
 
 func spawn_nodes():
 	var total_offset = 0
@@ -25,5 +47,9 @@ func spawn_nodes():
 		add_child(section)
 		sections_list.append(section)
 		section.position.y = total_offset
-		previous_section.get_node("PinJoint2D").node_b = previous_section.get_node("PinJoint2D").get_path_to(section)
+		var pin = PinJoint2D.new()
+		previous_section.add_child(pin)
+		pin.position = Vector2(0, 0)
+		pin.node_a = '../'
+		pin.node_b = pin.get_path_to(section)
 		previous_section = section
