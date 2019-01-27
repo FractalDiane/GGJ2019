cc50384deb78b2ccc3782db627e8d01514978ca8
Duncan Sparks <duncan.sprks@gmail.com>
2019-01-26 17:46:14 -0800

    Add exit from credits

---
+++ Scripts/Credits.gd
@@ -0,0 +1,5 @@
+extends Node2D
+
+func _process(delta):
+	if Input.is_action_just_pressed("ui_click") or Input.is_action_just_pressed("ui_gamepad_click"):
+		Controller.change_scene("res://Scenes/TitleScreen.tscn")
