467a6981eb57691080f66646e8d7a1dfd1dd8788
turtlewit <turtlewit@gmail.com>
2019-01-27 14:56:23 -0800

    maybe fix bug on bottom

--- Scripts/PlayerCamera.gd
+++ Scripts/PlayerCamera.gd
@@ -10,10 +10,14 @@ extends Camera2D
 
 export var camera_offset = 160
 
+var player_is_in = false
+
 func _physics_process(delta):
 	var player = Controller.get_player()
 	if player != null:
 		position.x = lerp(position.x, Controller.get_player().position.x - camera_offset, 1)
+	if player_is_in:
+		player.linear_velocity.y = -50
 
 
 func _on_StaticBody2D_body_entered(body):
@@ -32,6 +36,12 @@ func _on_CameraView_body_entered(body):
 
 func _on_StaticBody2D2_body_entered(body):
 	if "Player" in body.get_groups():
+		player_is_in = true
 		body.linear_velocity.y = -50
 		body.animator.play("jump")
 		body.get_node("SoundBounce").play()
+
+
+func _on_StaticBody2D2_body_exited(body):
+	if "Player" in body.get_groups():
+		player_is_in = false
